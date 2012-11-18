a6c7277e4191385fadb41494e87aa4b2e2e99a9a
Niraj Swami <fivesquared@gmail.com>
2012-11-18 12:58:24 -0600

    JSK!

--- public/paste.php
+++ public/paste.php
@@ -10,7 +10,7 @@
   <!-- Set the viewport width to device width for mobile -->
   <meta name="viewport" content="width=device-width" />
 
-  <title></title>
+  <title>paste your ingredients</title>
 
   <!-- Included CSS Files -->
   <link rel="stylesheet" href="stylesheets/foundation.css">
@@ -43,53 +43,17 @@
   <br/>
   <br/>
   <div class="row" align="center">
-	<div class="six columns">
+	<div class="two columns"></div>
+	<div class="eight columns">
 		<div class="panel radius">
-			<p class="lead">grab and drag the link below to your bookmarks</p>
-			<h3><a href="javascript:void((function()%7Bvar%20e%3Ddocument.createElement(%27script%27)%3Be.setAttribute(%27type%27,%27text/javascript%27)%3Be.setAttribute(%27charset%27,%27UTF-8%27)%3Be.setAttribute(%27src%27,%27http://couponmyrecipe.orchestra.io/swami-bmklt.js%3Fr%3D%27%2BMath.random()*99999999)%3Bdocument.body.appendChild(e)%7D)())%3B"><u>CouponMyRecipe!</u></a></h3>
+			<h3>paste your ingredients below</h3>
+			<p class="lead">
+				<textarea id="ingredientPaste" width="100%" height="150px"></textarea>
+			</p>
+			<a href="#" class="button medium radius">find deals</a>
 		</div>
 	</div>
-	<div class="six columns">
-		<div class="callout panel radius">
-			<p class="lead">or, you can just copy and paste your ingredients</p>
-			<a class="success button medium" href="paste.php">click to paste</a>
-		</div>
-	</div>
-  </div>
-  <div class="row">
-	<div class="six columns">
-		<div class="callout panel">
-			<h3 style="margin-bottom:2px;">Instructions</h3>
-			<h6 style="margin-top:2px;">(or watch video on the right)</h6>
-			<ol style="margin:0px 20px 0px 20px; font-size:14pt;">
-			  <li>click and drag the 'CouponMyRecipe!' link above to your bookmarks bar (or folder)</li>
-			  <li>click on one of the logos below to visit the recipe website and look for the recipe of your choice</li>
-			  <li>once on a recipe page, click on the 'CouponMyRecipe!' bookmark from your bar (or folder)</li>
-			  <li>you should see a pop-up with a list of coupons found for ingredients in your recipe</li>
-			  <li>select the coupons you'd like to use and print!</li>
-			  <li><strong>let us know what you think in the survey below</strong></li>
-			</ol>
-		</div>
-	</div>
-	<div class="six columns">
-		<div class="flex-video">
-		  <iframe width="420" height="315" src="http://www.youtube.com/embed/Kzv6H831JGw" frameborder="0" allowfullscreen></iframe>
-		</div>
-	</div>
-  </div>
-  <div class="row">
-  	<div class="twelve columns">
-		<a href="http://www.epicurious.com/recipes/food/views/Grass-Fed-Beef-Meatloaf-in-a-Bacon-Blanket-368549" target="_blank"><img style="width:195px; height:55px;" src="./images/printer_friendly_epi_logo.gif" /></a>&nbsp;&nbsp;
-  		<a href="http://www.foodnetwork.com/recipes/ina-garten/lemon-chicken-breasts-recipe/index.html" target="_blank"><img style="width:55px; height:55px;" src="./images/FoodNetworkLogo.jpeg" /></a>&nbsp;&nbsp;
-  		<a href="http://allrecipes.com/recipe/sausage-pasta/detail.aspx?event8=1&prop24=SR_Title&e11=pasta&e8=Quick%20Search&event10=1&e7=Home%20Page" target="_blank"><img style="width:105px; height:55px;" src="./images/ar_logo.gif" /></a>
-  	</div>
-  </div>
-  <br/>
-  <div class="row">
-	<div class="panel">
-		<h3>Survey</h3>
-		<iframe src="https://docs.google.com/spreadsheet/embeddedform?formkey=dHE5Z29EVkhqZWxwQlBwSW9TRWM0ZlE6MQ" width="100%" height="650" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
-	</div>
+	<div class="two columns"></div>
   </div>
   <br/>
   <div class="row" align="center">
